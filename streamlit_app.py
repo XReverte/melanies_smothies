a45fe8852d3c96f6e65b2ce18e88c6000c7ccf8a
Francesc Xavier Reverté Baró <100844285+XReverte@users.noreply.github.com>
2025-03-04 17:31:45 +0100

    Update streamlit_app.py
    SEARCH_NAME

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,9 @@ st.write('The name on ypur smoothie will be:', name_on_order)
 
 cnx=st.connection("snowflake")
 session = cnx.session()# Get the current credentials
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True) # Shows the dataframe on the page
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True) # Shows the dataframe on the page
+st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5) # Select using dataframe, max=5
 if ingredients_list:
