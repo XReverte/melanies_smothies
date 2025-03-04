9705feed407fa167bd860910c6e449b7305a66ce
Francesc Xavier Reverté Baró <100844285+XReverte@users.noreply.github.com>
2025-03-04 16:06:57 +0100

    Create streamlit_app.py
    Copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,40 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
+st.write("""Choose the fruits you want in your smooothie!""")
+
+#rdc = st.selectbox('Root Depth:', ('S','M','D'))
+
+name_on_order = st.text_input('Name on Smoothie')
+st.write('The name on ypur smoothie will be:', name_on_order)
+
+session = get_active_session()# Get the current credentials
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True) # Shows the dataframe on the page
+
+ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5) # Select using dataframe, max=5
+if ingredients_list:
+    #st.text(ingredients_list) # Show the list in constraint format
+    #st.write(ingredients_list) # Show the list in desglosed format
+    
+    # Convert a list to a string
+    ingredients_string=''
+    for fruit_chosen in ingredients_list:
+        ingredients_string+=fruit_chosen + ' '
+
+    #my_insert_stmt = """ INSERT INTO smoothies.public.orders(ingredients,NAME_ON_ORDER)
+            #values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    my_insert_stmt = f""" INSERT INTO smoothies.public.orders(ingredients,NAME_ON_ORDER)
+            values ('{ingredients_string}','{name_on_order}')"""
+
+    st.write(my_insert_stmt)
+    #st.stop() # To stop the execution here
+    
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
